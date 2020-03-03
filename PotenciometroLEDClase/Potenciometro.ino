e534af39bf5a8873a7ff903cffa6fac04812645f
luispri2001 <55539850+luispri2001@users.noreply.github.com>
2020-03-03 13:57:39 +0100

    Clase 2

---
+++ PotenciometroLEDClase/Potenciometro.ino
@@ -0,0 +1,14 @@
+// the setup routine runs once when you press reset:
+void setup() {
+  // initialize serial communication at 9600 bits per second:
+  Serial.begin(9600);
+}
+
+// the loop routine runs over and over again forever:
+void loop() {
+  // read the input on analog pin 0:
+  int sensorValue = analogRead(A0);
+  // Convert the analog reading (which goes from 0 - 1023) to a voltage (0 - 5V):
+  float voltage = sensorValue * (254.0 / 1023.0);
+   analogWrite(9, voltage);
+}
